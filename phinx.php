3e8d37bf2366c3994974710c44e604efa202c3d0
Cyril Rouyer <cyril@netlor.fr>
2018-07-06 17:51:27 +0200

    Attempt to allow extra param configs

--- phinx.php
+++ phinx.php
@@ -3,6 +3,13 @@ require_once __DIR__.'/../../../config/config.php';
 if(file_exists(__DIR__.'/../../../config/default.php')){
     require_once __DIR__.'/../../../config/default.php';
 }
+if(defined('EXTRA_CONFIGS') && is_array(EXTRA_CONFIGS)) {
+    foreach(EXTRA_CONFIGS as $conf) {
+        if(file_exists(__DIR__.'/../../../'.$conf)) {
+            require_once __DIR__.'/../../../'.$conf;
+        }
+    }
+}
 return array(
     'paths' => array(
         'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
