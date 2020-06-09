1cfd23ab8ce28e5ebe385c122abfe60f3fe31a6e
Ti-Tom <thomas@laella.net>
2020-06-09 11:29:15 +0200

    FIX: Phinx migration up/down

--- db/migrations/20180927071437_allow_null_on_docs_updated_at_cols.php
+++ db/migrations/20180927071437_allow_null_on_docs_updated_at_cols.php
@@ -2,8 +2,10 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class AllowNullOnDocsUpdatedAtCols extends AbstractMigration {
-    public function change() {
+class AllowNullOnDocsUpdatedAtCols extends AbstractMigration
+{
+    public function up()
+    {
         $this->table('documents')
             ->changeColumn('updated_at', 'datetime', ['null' => true])
             ->changeColumn('updated_by', 'string', ['null' => true])
@@ -11,6 +13,17 @@ class AllowNullOnDocsUpdatedAtCols extends AbstractMigration {
         $this->table('folders')
             ->changeColumn('updated_at', 'datetime', ['null' => true])
             ->changeColumn('updated_by', 'string', ['null' => true])
+            ->update();
+    }
+    public function down()
+    {
+        $this->table('documents')
+            ->changeColumn('updated_at', 'datetime')
+            ->changeColumn('updated_by', 'string')
+            ->update();
+        $this->table('folders')
+            ->changeColumn('updated_at', 'datetime')
+            ->changeColumn('updated_by', 'string')
             ->update();
     }
 }
