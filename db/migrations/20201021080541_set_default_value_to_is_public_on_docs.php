c2a1f0863245f5c698cfd24d52eee1537f70a2c7
Cyril Rouyer <cyril@netlor.fr>
2020-10-21 10:14:43 +0200

    Fix default value on is_public to false

---
+++ db/migrations/20201021080541_set_default_value_to_is_public_on_docs.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class SetDefaultValueToIsPublicOnDocs extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $this->table('documents')
+             ->changeColumn('is_public', 'boolean', ['default' => false])
+             ->update();
+    }
+
+    public function down()
+    {
+        $this->table('documents')
+             ->changeColumn('is_public', 'boolean')
+             ->update();
+    }
+}
