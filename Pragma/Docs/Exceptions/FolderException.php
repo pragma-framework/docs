6613c36b7a7063a15c856639008296cad3014555
Cyril Rouyer <cyril@netlor.fr>
2022-01-04 18:02:46 +0100

    breaking EVO : Pragma won't check unicity of folder's names anymore

--- Pragma/Docs/Exceptions/FolderException.php
+++ Pragma/Docs/Exceptions/FolderException.php
@@ -6,7 +6,6 @@ class FolderException extends \Exception{
 	const DUPLICATE_NAME_ERROR = 2;
 
 	const EMPTY_NAME_MSG = 'This folder\'s name cannot be empty';
-	const DUPLICATE_NAME_MSG = 'This folder\'s name is already used in this folder';
 
 	public function __toString(){
 		return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
